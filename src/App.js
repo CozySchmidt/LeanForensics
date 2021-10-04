1dbdc96591251a444b77b4b1fadc16d5e799a05a
Trevor Schmidt <t10.schmidt@gmail.com>
2021-10-03 21:40:57 -0700

    Connected app to database in AWS for testing purposes (#2)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+ 
+constructor(props){
+
+  super(props)
+  this.state={
+    sampleid:"",
+    sampletype:""
+
+  }
 }
 
+handleChange= (event) =>{
+  event.preventDefault();
+  const {sampleid, value} = event.target;
+  this.setState({[sampleid]:value});
+  console.log(this.state);
+}
+  render(){
+    return(
+      <div className="App">
+        <form method="post" action="http://localhost:3001">
+          <div className="sampleid">
+            <label htmlFor='sampleid'>Enter Sample ID:</label>
+            <input type="text" name="sampleid" onChange={this.handleChange}/>
+          </div>
+          <div className="sampletype">
+            <label htmlFor='sampletype'>Enter Sample Type:</label>
+            <input type="text" name="sampletype" onChange={this.handleChange}/>
+          </div>
+          <div className="submit">
+            <input type="submit"/>
+          </div>
+        </form>
+      </div>
+    );
+}
+}
 export default App;

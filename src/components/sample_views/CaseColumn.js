c3339bd99c45a93e705cd06ccec62be0e55e4cfd
Joshua Jang <joshua.jang429@gmail.com>
2021-11-06 05:48:56 -0700

    Case View implemented

---
+++ src/components/sample_views/CaseColumn.js
@@ -0,0 +1,24 @@
+export const COLUMNS = [
+    {
+        Header: 'Case Number',
+        accessor: 'caseNumber',
+    }, 
+    {
+        Header: 'Current Status',
+        accessor: 'currentStatus',
+    },
+    {
+        Header: 'Submitted On',
+        accessor: 'submitted',
+    }, 
+    {
+        Header: 'Created On',
+        accessor: 'created',
+    }, 
+    {
+        Header: 'Reported On',
+        accessor: 'reported',
+    }
+]
+
+export default COLUMNS;

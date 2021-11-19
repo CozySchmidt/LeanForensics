f0af1a53145c9442903d9f065b198834243392d4
joshuajang429 <59620723+joshuajang429@users.noreply.github.com>
2021-11-18 23:29:58 -0800

    Jj/api view connect (#19)
    * implemented API calling and successful connection with DB
    
    * package-lock and package.json updated

--- src/components/sample_views/SampleView.js
+++ src/components/sample_views/SampleView.js
@@ -1,103 +1,90 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { useTable, useGlobalFilter, useSortBy } from 'react-table';
-import MOCK_DATA from '../../constants/mock_samples.json';
-import COLUMNS from './Columns';
+import MOCK_DATA from "../../constants/mock_samples.json";
+import Button from "@mui/material/Button";
 import './SampleView.css';
+import Table from "./Table";
 import { GlobalFilter } from "./GlobalFilter";
 import {BsFillArrowDownCircleFill, BsFillArrowUpCircleFill} from "react-icons/bs";
 
+import {getAllSamples} from "../../api/SampleApi";
+import axios from "axios";
+
 const SampleView = () => {
 
-  const data = useMemo(() => MOCK_DATA, []);
-  const columns = useMemo(() => COLUMNS, []);
+  let [sampleList, setSampleList] = useState(null);
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    state,
-    setGlobalFilter,
-  } = useTable({ columns, data }, useGlobalFilter, useSortBy);
+  useEffect(() => {
+    retrieveSamples();
+  }, []);
 
-  const { globalFilter } = state;
+  async function retrieveSamples() {
+    let sampleList = await getAllSamples();
+    console.log(sampleList);
+    setSampleList(sampleList);
+  }
 
+  const columns = useMemo(() => [
+    {
+      accessor: "SampleId",
+      Header: "Sample ID",
+      width: 50,
+    },
+    {
+      accessor: "ScreeningId",
+      Header: "Screening ID",
+      width: 200,
+    },
+    {
+      accessor: "ScreeningName",
+      Header: "Screening Name",
+      width: 90,
+    },
+    {
+      accessor: "KitId",
+      Header: "Kit Id",
+      width: 150,
+    },
+    {
+      accessor: "KitName",
+      Header: "Kit Name",
+      width: 110,
+    },
+    {
+      accessor: "CaseId",
+      Header: "Case ID",
+      width: 110,
+    },
+    {
+      accessor: "OnHold",
+      Header: "On Hold",
+      width: 150,
+      renderCell: (cellValues) => {
+        return (
+          cellValues === 1 && (
+            <Button variant="contained" color="warning">
+              On Hold
+            </Button>
+          )
+        );
+      },
+    },
+    {
+      accessor: "CreatedDate",
+      Header: "Created Date",
+      width: 110,
+    },
+  ], []);
+
+  while (sampleList == null) {
+    return (<div></div>)
+  }
+  
   return (
     <div className="viewHolder">
-      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter}/>
-      <table className="table--samples" {...getTableProps()}>
-        <thead className="table--samples--header">
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
-                  <span className="table--samples--header--sorted">
-                    {column.isSorted ? (column.isSortedDesc ? <BsFillArrowDownCircleFill/> : <BsFillArrowUpCircleFill/>) : ''}
-                  </span>
-                </th>
-              ))}
-              <th>Edit</th>
-            </tr>
-          ))}
-        </thead>
-        <tbody className="table--samples--body" {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-                <td>
-                  <button onClick={() => {
-                    alert(`Do you wish to edit ${row.values['sampleNumber']}?`)
-                  }}>Edit</button>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <Table columns={columns} data={sampleList}></Table>
     </div>
   )
-
-  
-  // const [samples, setSamples] = useState(MOCK_DATA);
-
-  // return (
-  //   <div className="samples--container">
-  //     <Filter></Filter>
-  //     <table className="table--samples">
-  //       <thead className="table--samples--header">
-  //         <tr>
-  //           <th>Sample Number</th>
-  //           <th>Current Status</th>
-  //           <th>Case Number</th>
-  //           <th>Batch Number</th>
-  //           <th>Submitted On</th>
-  //           <th>Created On</th>
-  //           <th>Reported On</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody className="table--samples--body">
-  //         {samples.map((sample) => (
-  //           <tr>
-  //             <td>{sample.sampleNumber}</td>
-  //             <td>{sample.currentStatus}</td>
-  //             <td>{sample.caseNumber}</td>
-  //             <td>{sample.batchNumber}</td>
-  //             <td>{sample.submitted}</td>
-  //             <td>{sample.created}</td>
-  //             <td>{sample.reported}</td>
-  //           </tr>
-  //         ))}
-  //       </tbody>
-  //     </table>
-  //   </div>
-  // );
-
 };
 
-export default SampleView;
+export default SampleView
